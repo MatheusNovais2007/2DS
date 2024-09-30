951f3da1b41dcdc6fdf3ae78be7f8dfcae7dbbed
MatheusNovais2007 <matheus.novais162007@gmail.com>
2024-09-30 19:05:48 -0300

    palavra secreta

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 def descobrir_palavra_secreta(palavra_secreta):
-   # Iniciar uma lista para armazenar as letra adivinhadas corretamente
+   
   palavra_descoberta = ['_' for _ in palavra_secreta]
 
-  # DEfinir o numero maximo de tentativas
+  
   tentativas_restantes = 0
 
   # Loop ate que a palavra seja descoberta
 
-  # Solicitar uma letra ao usuario
+ 
   letra = input("Digite uma letra:").lower()
 
-  #verificar se a letra esta na palavra secreta
+  
   if letra in palavra_secreta:
      print(f"Boa! A letra'{letra}' esta na palavra")
-     # Substituir os underlines pelas letras correspondentes
+     
      for i in range(len(palavra_secreta)):
          if palavra_secreta[i]== letra:
 
@@ -21,7 +21,7 @@ def descobrir_palavra_secreta(palavra_secreta):
          else:
           print("A letra não esta na palavra")    
 
-  # Verificar se o usuario descobriu a palavra
+  
   if '_' not in palavra_descoberta:
         print(f"Você descobriu a palavra:{''.join(palavra_descoberta)}")
 
